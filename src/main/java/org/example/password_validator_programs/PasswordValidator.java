c620e3a7a83f9433a5b9f9a6434d5053af4fb2c4
juwoong <wwong7247@gmail.com>
2022-10-25 00:01:25 +0900

    [password_validator_programs] 요구사항 인덱스 수정 및 1번기능 test code 작성 및 validate 기능 구현

--- src/main/java/org/example/password_validator_programs/PasswordValidator.java
+++ src/main/java/org/example/password_validator_programs/PasswordValidator.java
@@ -1,4 +1,13 @@
 package org.example.password_validator_programs;
 
 public class PasswordValidator {
+
+    public void validator(String password) {
+
+        int length = password.length();
+
+        if(length<8 && length >12){
+            throw new IllegalArgumentException("올바른 패스워드가 아닙니다");
+        }
+    }
 }
--- src/test/java/org/example/password_validator_programs/PasswordValidatorTest.java
+++ src/test/java/org/example/password_validator_programs/PasswordValidatorTest.java
@@ -2,14 +2,15 @@ package org.example.password_validator_programs;
 
 /*
 요구사항
-• 비밀번호는 최소 8자 이상 12자 이하여야 한다.
-• 비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외를 발생 시킨다.
-• 경계조건에 대해 테스트 코드를 작성해야 한다
+1. 비밀번호는 최소 8자 이상 12자 이하여야 한다.
+2. 비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외를 발생 시킨다.
+3. 경계조건에 대해 테스트 코드를 작성해야 한다
  */
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class PasswordValidatorTest {
@@ -18,5 +19,15 @@ public class PasswordValidatorTest {
     @Test
     void createTest() {
         assertThatCode(()-> new PasswordValidator()).doesNotThrowAnyException();
+    }
+
+    @DisplayName("비밀번호는 최소 8자 이상 12자 이하여야 한다. 조건이 맞을때 통과시키는 기능 테스트")
+    @Test
+    void pass_validate_test() {
+
+        PasswordValidator passwordValidator = new PasswordValidator();
+        String passPassword = "12345678";
+        assertThatCode(() ->passwordValidator.validator(passPassword)).doesNotThrowAnyException();
+
     }
 }
